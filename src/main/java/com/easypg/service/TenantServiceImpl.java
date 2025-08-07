44a232bec21d47d1977c559d62060e2cd370907a
Tanvi Gavhane <gavhanetanvi@gmail.com>
2025-08-07 08:43:43 +0530

    "Tenant PATCH AND DELETE"

--- src/main/java/com/easypg/service/TenantServiceImpl.java
+++ src/main/java/com/easypg/service/TenantServiceImpl.java
@@ -1,6 +1,7 @@
 package com.easypg.service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Period;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,6 +19,7 @@ import com.easypg.dao.UserDao;
 import com.easypg.dto.AddTenantDTO;
 import com.easypg.dto.ApiResponse;
 import com.easypg.dto.TenantResponseDTO;
+import com.easypg.dto.UpdateTenantDTO;
 import com.easypg.entities.Complaint;
 import com.easypg.entities.Room;
 import com.easypg.entities.Tenant;
@@ -74,18 +76,16 @@ public class TenantServiceImpl implements TenantService{
 				                              .map((tenant) -> {
 		TenantResponseDTO dto = mapper.map(tenant, TenantResponseDTO.class);
 		
-		Room room = roomDao.findById(tenant.getRoom().getId()).orElseThrow(() -> new ResourceNotFoundException("No such room exists"));
+		Room room = tenant.getRoom();
 				
 		List<Complaint> complaints = complaintDao.findByTenantId(tenant.getId());
         int totalComplaints = complaints.size();
         int activeComplaints = (int) complaints.stream()
-                .filter(c -> "Active".equalsIgnoreCase(c.getComplaintStatus().toString()))
+                .filter(c -> "IN_PROGRESS".equalsIgnoreCase(c.getComplaintStatus().toString()))
                 .count();
-        
-        LocalDate moveInDate = tenant.getMoveInDate();
-        LocalDate currentDate = LocalDate.now();
+      
 
-        Period period = Period.between(moveInDate, currentDate);
+        Period period = Period.between(tenant.getMoveInDate(), LocalDate.now());
         int tenureInMonths = period.getYears() * 12 + period.getMonths();
 
         dto.setUsername(tenant.getUser().getUsername());
@@ -100,6 +100,52 @@ public class TenantServiceImpl implements TenantService{
 				                              
 		return tenantsList;
 	}
-	
+
+
+	@Override
+	public ApiResponse updateTenant(Long tenantId, UpdateTenantDTO dto) {
+        Tenant tenant = tenantDao.findById(tenantId)
+                .orElseThrow(() -> new ResourceNotFoundException("Tenant not found"));
+        User user = tenant.getUser();
+        if (dto.getEmail() != null) {
+        	user.setEmail(dto.getEmail());
+        }
+
+        if (dto.getContactNumber() != null) {
+        	tenant.setContactNumber(dto.getContactNumber());
+        }
+        
+        if (dto.getRoomNumber() != null) {
+        	 Room newRoom = roomDao.findByRoomNo(dto.getRoomNumber())
+                     .orElseThrow(() -> new ResourceNotFoundException("Room not found with number: " + dto.getRoomNumber()));
+
+             tenant.setRoom(newRoom); 
+        }
+
+        tenant.setUpdatedAt(LocalDateTime.now());
+
+        tenantDao.save(tenant);
+        
+        return new ApiResponse("Tenant updated successfully with ID : "+tenant.getId());
+    }
+
+
+	@Override
+	public ApiResponse deleteTenant(Long tenantId) {
+	    Tenant tenant = tenantDao.findById(tenantId)
+	            .orElseThrow(() -> new ResourceNotFoundException("Tenant not found with ID: " + tenantId));
+
+	    if (tenant.isDeleted()) {
+	        return new ApiResponse("Tenant is already deleted with ID: " + tenantId);
+	    }
+
+	    tenant.setDeleted(true);  // soft delete -> only setting isDeleted to true
+	    tenant.setUpdatedAt(LocalDateTime.now());
+
+	    tenantDao.save(tenant);
+
+	    return new ApiResponse("Tenant soft deleted successfully with ID: " + tenantId);
+	}
+
 	
 }
